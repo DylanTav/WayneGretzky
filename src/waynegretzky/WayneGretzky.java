d95326d7c7566baa01ea4284e0987f2842ed52f1
dytav9540 <dytav9540@7KZL6Y1.Student.UGDSB.ED>
2018-02-28 11:39:50 -0500

    This is my second commit where I fixed my header

---
+++ src/waynegretzky/WayneGretzky.java
@@ -0,0 +1,23 @@
+/*
+This program was made by Dylan T
+On February 28th, 2018
+To display Wayne Gretzky and his number on a seperate line
+*/
+package waynegretzky;
+
+
+public class WayneGretzky {
+
+    
+    public static void main(String[] args) {
+        
+        String Wayne; // To create a String called Wayne
+        String Num; // To create a String called Num
+       
+        Num = "99"; // Num holds a value of 99
+        Wayne = "Wayne Gretzky"; // Wayne holds Wayne Gretzky in the varaible
+        
+        System.out.println(Wayne + " is number " + Num); //prints Wayne to the screen 
+    
+}
+}
